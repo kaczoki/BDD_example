84a4d35b586574bef3da35b209fbd15653ec76d8
Mariusz Kapczyński <mariusz.kapczynski@huuugegames.com>
2020-08-02 14:22:22 +0200

    Added params in step definitions

--- src/test/java/stepdefinitions/maps/google/GoogleMaps.java
+++ src/test/java/stepdefinitions/maps/google/GoogleMaps.java
@@ -32,27 +32,21 @@ public class GoogleMaps {
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     }
 
-    @When("^I search for Berlin Germany$")
-    public void i_search_for_Berlin_Germany() throws Throwable {
-        driver.findElement(By.id("searchboxinput")).sendKeys("Berlin, Germany"+ Keys.ENTER);
+    @When("^I search for (.*?)$")
+    public void i_search_for_city(String city) throws Throwable {
+        driver.findElement(By.id("searchboxinput")).sendKeys(city + Keys.ENTER);
 
     }
 
-    @Then("^I should see Berlin on the map$")
-    public void i_should_see_Berlin_on_the_map() throws Throwable {
-        driver.findElement(By.xpath("//h1/span[contains(text(),'Berlin')]"));
+    @Then("^I should see (.*?) on the map$")
+    public void i_should_see_city_on_the_map(String city) throws Throwable {
+        driver.findElement(By.xpath("//h1/span[contains(text(),'"+city+"')]"));
+
         Thread.sleep(4000);
     }
 
-    @When("^I search for pizza in Berlin$")
-    public void iSearchForPizzaInBerlin() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
-    }
-
-    @Then("^I should see pizza restaurant on the map$")
-    public void iShouldSeePizzaRestaurantOnTheMap() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    @Then("^I should see pizza restaurant the map$")
+    public void iShouldSeePizzaRestaurantTheMap() throws Throwable {
+        Thread.sleep(4000);
     }
 }
